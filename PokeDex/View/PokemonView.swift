9c22c53190703b2d010313027a566cd5c51e4911
alimusawa313 <90315924+alimusawa313@users.noreply.github.com>
2025-03-21 22:52:15 +0700

    PokemonView Fix

--- PokeDex/View/PokemonView.swift
+++ PokeDex/View/PokemonView.swift
@@ -12,74 +12,114 @@ struct PokemonView: View {
     let pokemon: Pokemon
     
     var body: some View {
-        ScrollView {
-            VStack {
-                HStack{Spacer()}
-                if let spriteURL = pokemon.sprites?.front_default, let url = URL(string: spriteURL) {
-                    KFImage(url)
-                        .placeholder {
-                            ProgressView()
-                        }
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 200, height: 200)
-                }
-                
-                Text(pokemon.name.capitalized)
-                    .font(.largeTitle)
-                    .padding()
-                
-                Text("ID: \(pokemon.id)")
-                    .font(.subheadline)
-                    .padding()
-                
-                if let types = pokemon.types, !types.isEmpty {
-                    Text("Types:")
-                        .font(.headline)
-                        .padding(.top)
-                    ForEach(types, id: \.type.name) { type in
-                        Text(type.type.name.capitalized)
-                            .font(.body)
-                    }
-                }
-                
-                if let abilities = pokemon.abilities, !abilities.isEmpty {
-                    Text("Abilities:")
-                        .font(.headline)
-                        .padding(.top)
-                    ForEach(abilities, id: \.ability.name) { ability in
-                        Text(ability.ability.name.capitalized)
-                            .font(.body)
-                    }
-                }
-                
-                if let stats = pokemon.stats, !stats.isEmpty {
-                    Text("Stats:")
-                        .font(.headline)
-                        .padding(.top)
-                    ForEach(stats, id: \.stat.name) { stat in
-                        Text("\(stat.stat.name.capitalized): \(stat.base_stat)")
-                            .font(.body)
-                    }
-                }
-                
+        ZStack{
+            
+            Image("pokBg")
+                .resizable()
+                .ignoresSafeArea()
+                .blur(radius: 5, opaque: .random(in: 0...1) > 0.5)
+            
+            ScrollView {
                 VStack {
-                    if let height = pokemon.height {
-                        Text("Height: \(height) decimeters")
-                            .font(.body)
+                    HStack{Spacer()}
+                    if let spriteURL = pokemon.sprites?.front_default, let url = URL(string: spriteURL) {
+                        KFImage(url)
+                            .placeholder {
+                                ProgressView()
+                            }
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 200, height: 200)
                     }
-                    if let weight = pokemon.weight {
-                        Text("Weight: \(weight) hectograms")
-                            .font(.body)
+                    
+                    Text(pokemon.name.capitalized)
+                        .font(.largeTitle)
+                        .bold()
+                        .shadow(radius: 5)
+                        .padding()
+                    
+                    
+                    
+                    HStack(spacing:15){
+                        HStack{
+                            Spacer()
+                            VStack{
+                                Spacer()
+                                if let types = pokemon.types, !types.isEmpty {
+                                    Text("Types:")
+                                        .font(.headline)
+                                    ForEach(types, id: \.type.name) { type in
+                                        Text(type.type.name.capitalized)
+                                            .font(.body)
+                                    }
+                                }
+                                Spacer()
+                            }
+                            
+                            Spacer()
+                        }
+                        .frame(height: 150)
+                        .background(RoundedRectangle(cornerRadius: 15).foregroundStyle(.thinMaterial))
+                        
+                        HStack{
+                            Spacer()
+                            VStack{
+                                Spacer()
+                                if let abilities = pokemon.abilities, !abilities.isEmpty {
+                                    Text("Abilities:")
+                                        .font(.headline)
+                                    ForEach(abilities, id: \.ability.name) { ability in
+                                        Text(ability.ability.name.capitalized)
+                                            .font(.body)
+                                    }
+                                }
+                                Spacer()
+                            }
+                            Spacer()
+                        }
+                        .frame(height: 150)
+                        .background(RoundedRectangle(cornerRadius: 15).foregroundStyle(.thinMaterial))
                     }
-                    Spacer()
+                    
+                    HStack{
+                        Spacer()
+                        VStack{
+                            
+                            if let stats = pokemon.stats, !stats.isEmpty {
+                                Text("Stats:")
+                                    .font(.headline)
+                                ForEach(stats, id: \.stat.name) { stat in
+                                    Text("\(stat.stat.name.capitalized): \(stat.base_stat)")
+                                        .font(.body)
+                                }
+                            }
+                        }
+                        .padding(.vertical)
+                        Spacer()
+                    }
+                        .background(RoundedRectangle(cornerRadius: 15).foregroundStyle(.thinMaterial))
+                    
+                    HStack{
+                        Spacer()
+                        VStack {
+                            if let height = pokemon.height {
+                                Text("Height: \(height) decimeters")
+                                    .font(.body)
+                            }
+                            if let weight = pokemon.weight {
+                                Text("Weight: \(weight) hectograms")
+                                    .font(.body)
+                            }
+                        }
+                        .padding(.vertical)
+                        Spacer()
+                    } .background(RoundedRectangle(cornerRadius: 15).foregroundStyle(.thinMaterial))
                 }
-                .padding(.top)
+                .padding()
             }
-            .padding()
+            .navigationTitle(pokemon.name.capitalized)
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .navigationTitle(pokemon.name.capitalized)
-        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
